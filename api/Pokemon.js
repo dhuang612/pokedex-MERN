4722bf98d32dfa36529593291f838c73399b18e9
dhuang612 <dhuang@madisoncollege.edu>
2019-06-13 14:26:07 -0400

    receiving pokemon data from api

--- api/Pokemon.js
+++ api/Pokemon.js
@@ -10,11 +10,20 @@ const options = {
   cacheLimit: 100 * 1000,
   timeout: 10 * 1000
 };
+/*
+can we push the results to an array and then iterate over to grab the info we want?
+
+*/
+const Pokedata = [];
 const Poke = new Pokedex(options);
 Poke.resource(['api/v2/pokedex/2/'])
   .then(function displayPokemon(req, res) {
+    // console.log(req[0].pokemon_entries);
     for (let i = 0; i < req.length; i++) {
-      console.log(req[i]);
+      if (req[i].pokemon_entries) {
+        Pokedata.push(req[i].pokemon_entries);
+        console.log(Pokedata);
+      }
     }
   })
   .catch(error => console.log(error));
